e1fe07f8b302dcd63e538f1e532e5aa40acc52f3
huangxuchu <huangxuchu@yiwafa.com>
2021-07-05 11:45:14 +0800

    修改app启动模式

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import Constants
 
+HOST = '192.168.31.34'
+PORT = 8883
+DEBUG = False
+
 
 # 配置对象，里面定义需要给 APP 添加的一系列配置
-class Config:
-    DEBUG = True
-    SERVER_NAME = "192.168.3.86:8887"
+class FlaskConfig:
+    DEBUG = DEBUG
+    SERVER_NAME = f'{HOST}:{PORT}'
     UPLOAD_FOLDER = Constants.PATH_MATERIAL
-
